3841b05252c41ff5960bcd086305d2dc3811881b
WYS <1785696428@qq.com>
2026-05-30 18:47:54 +0800

    删除可执行文件并且将修改了代码逻辑

--- DesignPattern/10_适配器模式/02_code.cpp
+++ DesignPattern/10_适配器模式/02_code.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 using namespace std;
 
+#if 0
+//适配器类和熊猫类是关联关系
 // 外国人
 class Foreigner
 {
@@ -97,6 +99,105 @@ public:
         m_foreigner->setResult("法国佬：" + msg);
     }
 };
+#endif
+
+#if 1
+//适配器和熊猫类是继承关系
+class Foreigner
+{
+public:
+    virtual string confession() = 0;
+    void setResult(string res)
+    {
+        cout << "Panda say:" << res << endl;
+    }
+    virtual ~Foreigner() {}
+};
+
+//美国人
+class American : public Foreigner
+{
+public:
+    string confession() override
+    {
+        return "american,我有罪\n";
+    }
+    
+};
+
+//法国人
+class French : public Foreigner
+{
+public:
+    string confession() override
+    {
+        return "french,我有罪\n";
+    }
+};
+
+//熊猫
+class Panda
+{
+public:
+    void recvMessage(string msg)
+    {
+        cout << msg << endl;
+    }
+    string sendMessage()
+    {
+        return string("强盗、罪人、凶手是不可能被宽恕和原谅的！\n");
+    }
+};
+
+//适配器类的基类
+class AbstractChopper : public Panda
+{
+public:
+    AbstractChopper(Foreigner* foreigner) : m_foreigner(foreigner) {}
+    virtual void translateToPandas() = 0;
+    virtual void translateToHuman() = 0;
+    virtual ~AbstractChopper() {}
+protected:
+    Foreigner* m_foreigner;
+};
+
+class EnglishChopper : public AbstractChopper
+{
+public:
+    // 继承父类的所有构造函数
+    using AbstractChopper::AbstractChopper;
+    void translateToPandas()
+    {
+        string msg = m_foreigner->confession();
+        //解析熊猫的语言，并将数据传递给熊猫
+        recvMessage("美国人说：" + msg);
+    }
+    void translateToHuman()
+    {
+        string msg = sendMessage();
+        // 将熊猫语言转换成英语，再传递给外国人
+        m_foreigner->setResult("美国佬：" + msg);
+    }
+};
+
+class FrenchChopper : public AbstractChopper
+{
+public:
+    using AbstractChopper::AbstractChopper;
+    void translateToPandas()
+    {
+        string msg = m_foreigner->confession();
+        //解析熊猫的语言，并将数据传递给熊猫
+        recvMessage("法国人说:" + msg);
+    }
+    void translateToHuman()
+    {
+        string msg = sendMessage();
+        // 将熊猫语言转换成英语，再传递给外国人
+        m_foreigner->setResult("法国佬：" + msg);
+    }
+};
+#endif
 
 int main()
 {
